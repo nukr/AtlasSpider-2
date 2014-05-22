a08bc976b37d4a8b611c9e5acf32775c64601da9
Nukr <nukrs.w@gmail.com>
2014-05-22 17:47:48 +0800

    add async lib

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var xml2js      = require('xml2js');
 var mongoose    = require('mongoose');
 var Coupon      = mongoose.model('Coupon');
 var ProgressBar = require('progress');
+var async = require('async');
 
 mongoose.connect('mongodb://admin:3345678@127.0.0.1/coupon', function (err) {
   if (err) {
@@ -13,6 +14,47 @@ mongoose.connect('mongodb://admin:3345678@127.0.0.1/coupon', function (err) {
     process.exit(1);
   }
 });
+
+function requestNuomiXML(callback) {
+    console.log('Starting download xml');
+    request.get('http://www.nuomi.com/api/dailydeal?version=v1&city=shanghai').pipe(fs.createWriteStream(__dirname + '/nuomi.xml'))
+    .on('close', function (err) {
+        console.log('Download Done');
+        callback();
+    });
+}
+
+function parseXML(callback){
+    console.log('Parsing XML ...');
+    var parser = new xml2js.Parser();
+    fs.readFile(__dirname + '/nuomi.xml', function (err, data) {
+        if (err) throw err;
+        parser.parseString(data, function (err, result) {
+            if (err) throw err;
+            console.log('Parse Done');
+            callback(null, result);
+        });
+    });
+}
+
+function isExist(coupon, callback) {
+    Coupon.find({web_url: coupon.loc}, function (err, doc) {
+        if (err) throw err;
+        if (doc.length === 0) {
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+function isExpire(date, callback) {
+    if (date < new Date()) {
+        callback(true);
+    } else {
+        callback(false);
+    }
+}
 
 function newCoupon(coupon) {
     var display = coupon.data[0].display[0]
@@ -33,55 +75,47 @@ function newCoupon(coupon) {
         //}
     });
 }
-function xml2db(onsuccess, onfailure) {
-    console.log('parsing xml and inserting into db');
-    var parser = new xml2js.Parser();
-    fs.readFile(__dirname + '/nuomi.xml', function (err, data) {
-        if (err) throw err;
-        parser.parseString(data, function (err, result) {
-            if (err) throw err;
-            var coupons = result.urlset.url;
-            var pending = coupons.length;
-            var bar = new ProgressBar('  processing [:bar] :percent :etas', {
-                complete: '=',
-                incomplete: ' ',
-                width: 20,
-                total: pending
-            });
-            coupons.forEach(function (coupon, i) {
-                Coupon.find({web_url: coupon.loc, title: coupon.data[0].display[0].title[0]}, function (err, doc) {
-                    if (doc.length === 0) { // doc is not found
-                        bar.tick(1)
-                        newCoupon(coupon);
-                        if (--pending === 0){
-                            console.log('done');
-                            mongoose.connection.close();
-                        }
-                    } else if (doc[0].expire < new Date()){
-                        doc[0].active = false;
-                        doc[0].last_updated = new Date();
-                        doc[0].save();
-                        bar.tick(1)
-                        if (--pending === 0){
-                            console.log('done');
-                            mongoose.connection.close();
-                        }
-                    } else {
-                        bar.tick(1)
-                        if (--pending === 0){
-                            console.log('done');
-                            mongoose.connection.close();
-                        }
-                    }
-                });
-            });
+
+function xml2db(coupons) {
+    var pending = coupons.length;
+    var bar = new ProgressBar('Inserting ... [:bar] :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total: pending
+    });
+    coupons.forEach(function (coupon, i) {
+        Coupon.find({web_url: coupon.loc, title: coupon.data[0].display[0].title[0]}, function (err, doc) {
+            if (doc.length === 0) { // doc is not found
+                newCoupon(coupon);
+                bar.tick(1);
+                if (--pending === 0){
+                    console.log('done');
+                    mongoose.connection.close();
+                }
+            } else if (doc[0].expire < new Date()){
+                doc[0].active = false;
+                doc[0].last_updated = new Date();
+                doc[0].save();
+                bar.tick(1);
+                if (--pending === 0){
+                    console.log('done');
+                    mongoose.connection.close();
+                }
+            } else {
+                bar.tick(1);
+                if (--pending === 0){
+                    console.log('done');
+                    mongoose.connection.close();
+                }
+            }
         });
-    })
+    });
 }
 
-console.log('starting download xml');
-request.get('http://www.nuomi.com/api/dailydeal?version=v1&city=shanghai').pipe(fs.createWriteStream(__dirname + '/nuomi.xml'))
-.on('close', function (err) {
-    console.log('download done');
-    xml2db();
+async.series([
+    requestNuomiXML,
+    parseXML
+], function (err, result) {
+    xml2db(result[1].urlset.url);
 });
